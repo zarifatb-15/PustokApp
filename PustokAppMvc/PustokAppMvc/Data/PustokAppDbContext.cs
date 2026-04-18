27325fb43cc24dac557712c939d01f3d4fe6b119
zarifatb-15 <zarifatb@code.edu.az>
2026-04-18 22:11:29 +0400

    added pustokappdbcontext

--- PustokAppMvc/PustokAppMvc/Data/PustokAppDbContext.cs
+++ PustokAppMvc/PustokAppMvc/Data/PustokAppDbContext.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using PustokAppMvc.Models;
 namespace PustokAppMvc.Data;
 
-public class PustokAppDbContext
+public class PustokAppDbContext(DbContextOptions<PustokAppDbContext>options):DbContext(options)
 {
-    
+    public DbSet <Slider> Sliders { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<BookTag> BookTags { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(PustokAppDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
 }
